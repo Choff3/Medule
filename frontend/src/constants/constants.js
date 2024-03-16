0083663e8477308c9ced1f317d4950883ed769e5
Choff3 <teddyhoff@gmail.com>
2024-03-16 15:13:04 -0400

    Delete old frontend

--- frontend/src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-export const POST = 'POST';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const PATIENT_ENDPOINT = process.env.REACT_APP_PATIENT_ENDPOINT || "http://localhost:5001/patient";
-export const MEDICATION_ENDPOINT = process.env.REACT_APP_MEDICATION_ENDPOINT || "http://localhost:5001/medication";
