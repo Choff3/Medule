174480159f76c51b9387aaac236168cd646fd2aa
Choff3 <teddyhoff@gmail.com>
2024-03-27 23:37:35 -0400

    Use AXIOS

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import patientRoutes from "./routes/patient.route.js";
 import medicationRoutes from "./routes/medication.route.js";
 
+const FHIR_SERVER = process.env.FHIR_SERVER_ENDPOINT
+
 const app = express();
 
-// dotenv.config();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/patient', patientRoutes);
 app.use('/medication', medicationRoutes);
+
+console.log(FHIR_SERVER);
+
+// TODO: Use axios to get all the patients from
+// FHIR_SERVER+"/Patient"
+// console.log(patients);
 
 app.listen(5001, () => {
     console.log("server has started on port 5001");
