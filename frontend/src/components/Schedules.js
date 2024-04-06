ddcf4e4ca7a88179174e296cf3e10677e9593bd6
Choff3 <teddyhoff@gmail.com>
2024-04-06 17:30:56 -0400

    routing works

--- frontend/src/components/Schedules.js
+++ frontend/src/components/Schedules.js
@@ -1,54 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Box } from "@mui/material";
 import axios from "axios";
 import {DataGrid} from "@mui/x-data-grid";
+import { useNavigate } from "react-router-dom";
 const SCHEDULE_ENDPOINT = "http://localhost:5001/schedule";
 
-class Schedules extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            schedules: {},
-            columns: [
-                { field: 'patientName', headerName: 'Patient', width: 150 },
-                { field: 'medication', headerName: 'Medications', width: 150 }
-            ]
-        };
-    }
+const Schedules = () => {
+    const [schedule, setSchedule] = useState(0);
+    const navigate = useNavigate();
+    const columns = [
+            { field: 'patientName', headerName: 'Patient', width: 200 },
+            { field: 'medication', headerName: 'Medications', width: 200 }
+    ];
 
-    async componentDidMount() {
+    useEffect(() => {
         axios({
             url: `${SCHEDULE_ENDPOINT}`,
             method: 'GET',
             headers: {"Content-Type": "application/json"}
         }).then(res => {
-            this.setState({ schedules: res.data })
+            setSchedule(res.data);
         })
-    };
+    });
 
-    // handleRowClick(event) {
-    //     // const navigate = useNavigate();
-    //     console.log(event.id);
-    //     const page = "/patient/"+event.id
-    //     this.props.navigation.navigate("/patient/"+event.id);
-    // };
-
-    render() {
-        return (
-            <Box m="20px">
-                <Box overflow="hidden">
-                    <DataGrid
-                        rows={this.state.schedules}
-                        columns={this.state.columns}
-                        autoHeight={true}
-                        getRowId={(row) => row._id?.$oid || row._id}
-                        // onRowClick={this.handleRowClick}
-                        onRowClick={(e) => navigation.navigate("/patient/"+e.id)}
-                    />
-                </Box>
+    return (
+        <Box m="20px">
+            <Box overflow="hidden">
+                <DataGrid
+                    rows={schedule}
+                    columns={columns}
+                    autoHeight={true}
+                    getRowId={(row) => row._id?.$oid || row._id}
+                    onRowClick={(e) => navigate("/patient/"+e.id)}
+                />
             </Box>
-        );
-    }
+        </Box>
+    );
 };
 
 export default Schedules;
