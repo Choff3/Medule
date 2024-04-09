058d3e27ad35643a4644b91f63925354e5d57654
Choff3 <teddyhoff@gmail.com>
2024-04-09 16:24:33 -0400

    Fetch Patient and Schedule

--- frontend/src/components/Patient.js
+++ frontend/src/components/Patient.js
@@ -1,22 +1,43 @@
 import React from "react";
 import { Box } from "@mui/material";
+import axios from "axios";
+const PATIENT_ENDPOINT = "http://localhost:5001/patient";
+const SCHEDULE_ENDPOINT = "http://localhost:5001/schedule";
 
 class Patient extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            patientId: window.location.href.split("/")[4]
+            patientId: window.location.href.split("/")[4],
+            patient: {},
+            schedule: {}
         }
     }
 
     async componentDidMount() {
-        console.log(this.state.patientId);
+        await axios({
+            url: `${PATIENT_ENDPOINT}`+"/"+this.state.patientId,
+            method: 'GET',
+            headers: {"Content-Type": "application/json"}
+        }).then(res => {
+            this.setState({patient: res.data[0].resource});
+        })
+        console.log(this.state.patient)
+
+        await axios({
+            url: `${SCHEDULE_ENDPOINT}`+"/"+this.state.patientId,
+            method: 'GET',
+            headers: {"Content-Type": "application/json"}
+        }).then(res => {
+            this.setState({schedule: res.data[0]});
+        })
+        console.log(this.state.schedule)
     };
 
     render() {
         return (
             <Box m="20px">
-                {this.state.patientId}
+                {this.state.schedule.patientName}
             </Box>
         );
     }
