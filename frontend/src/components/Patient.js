81eebb801f850bf29b801d75716606389eea2cb9
Choff3 <teddyhoff@gmail.com>
2024-04-09 16:10:58 -0400

    Get patient id in patient component

--- frontend/src/components/Patient.js
+++ frontend/src/components/Patient.js
@@ -4,17 +4,19 @@ import { Box } from "@mui/material";
 class Patient extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            patientId: window.location.href.split("/")[4]
+        }
     }
 
     async componentDidMount() {
-        console.log(this.props);
+        console.log(this.state.patientId);
     };
 
     render() {
         return (
             <Box m="20px">
-                hello
+                {this.state.patientId}
             </Box>
         );
     }
