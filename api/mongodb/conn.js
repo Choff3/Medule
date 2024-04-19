d831b7dc001f9e954a4934b5b644fe9066b3a7ab
Choff3 <teddyhoff@gmail.com>
2024-04-19 18:23:44 -0400

    Env vars fixed

--- api/mongodb/conn.js
+++ api/mongodb/conn.js
@@ -3,22 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || "medule";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "password";
-const MONGO_ENDPOINT = process.env.MONGO_ENDPOINT || "localhost:27017";
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'medule'
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'password'
+const MONGO_ENDPOINT = process.env.MONGO_ENDPOINT || 'localhost:27017'
 
 const connectionString = "mongodb://"+MONGO_USERNAME+":"+MONGO_PASSWORD+"@"+MONGO_ENDPOINT;
 
 const client = new MongoClient(connectionString);
 
-var conn;
+let conn;
 try {
     conn = await client.connect();
 } catch (e) {
     console.error(e);
 }
 
-var db = conn.db("medule");
+let db = conn.db("medule");
 
 export const patientCollection = db.collection("patient");
 export const medicationCollection = db.collection("medication");
